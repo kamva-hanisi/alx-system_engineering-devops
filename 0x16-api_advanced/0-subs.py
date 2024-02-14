8c4105b8234176fa0be8b6916ac638d03c5f3144
kamva-hanisi <lucashanisi@gmail.com>
2024-02-14 00:37:26 -0800

    Reddit API and returns the number

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Module to write function number_of_subscribers"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    """Function that queries the Reddit API and returns the
+        number of subscribers for a given subreddit
+    """
+    user_agent = 'Custom User Agent'
+    headers = {'User-Agent': user_agent}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200 and not response.is_redirect:
+        data = response.json()
+        subscribers_count = data['data']['subscribers']
+        return subscribers_count
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
